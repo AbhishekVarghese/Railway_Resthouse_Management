62ef6f4f70eca1300993933e1e219e8f0cdf04bb
Abhishek Varghese <27abhisonu98@gmail.com>
2020-08-28 19:22:37 +0530

    Improving File Structure

--- guest/connect1.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "sih";
-    $password = "sih@123";
-    $dbname="sih";
-
-    $conn = mysqli_connect($servername,$username,$password,$dbname);
-?>
